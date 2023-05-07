6a62779c561d3bbd47ff2c5da0c9a8cfbbc86d66
Crii-CL <crisrecon14@gmail.com>
2023-05-07 17:06:26 -0400

    .

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -8,3 +8,9 @@ const {
 router.get("/items", getClothingItems);
 router.post("/items", createClothingItem);
 router.delete("/items/:itemId", removeClothingItem);
+
+router.use("*", (req, res) => {
+  res.status(404).send({ message: "Requested resource not found" });
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ router.get("/users/:userId", getUser);
 router.get("/users", getUsers);
 router.post("/users", createUser);
 
+router.use("*", (req, res) => {
+  res.status(404).send({ message: "Requested resource not found" });
+});
+
 module.exports = router;
