ded1c44ef7045c205c16558685e9142e01b995a6
Crii-CL <crisrecon14@gmail.com>
2023-05-07 18:56:44 -0400

    .

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
+const bodyParser = require("mongoose");
 const userRoutes = require("./routes/users");
 const clothingItemsRoutes = require("./routes/clothingItems");
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3001, BASE_PATH } = process.env;
+const app = express();
 
-app.listen(PORT, () => {
-  console.log("App listening on port 3001");
+//wtwr_db is the name of the database you connect to. However, you can choose your own database name, if you prefer.
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db"); //wtwr_db is the name of the database you connect to. However, you can choose your own database name, if you prefer.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("./routes/users", userRoutes);
+app.use("./routes/clothingItems", clothingItemsRoutes);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.listen(PORT, () => {
+  console.log("App listening on port 3001");
+  console.log(BASE_PATH);
+});
