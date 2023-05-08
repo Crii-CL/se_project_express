9ab4250e5c931ee26f395da1b3cbb582cdfae94c
Crii-CL <crisrecon14@gmail.com>
2023-05-08 15:12:02 -0400

    fix the routes

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
+const http = require("http");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("mongoose");
 const userRoutes = require("./routes/users");
+const path = require("path");
 const clothingItemsRoutes = require("./routes/clothingItems");
 
-const { PORT = 3001, BASE_PATH } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 //wtwr_db is the name of the database you connect to. However, you can choose your own database name, if you prefer.
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("./routes/users", userRoutes);
+app.use("/users", userRoutes);
 app.use("./routes/clothingItems", clothingItemsRoutes);
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "",
+  };
+  next();
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.listen(PORT, () => {
   console.log("App listening on port 3001");
-  console.log(BASE_PATH);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const { getUser, getUsers, createUser } = require("../controllers/users");
 
-router.get("/users/:userId", getUser);
-router.get("/users", getUsers);
-router.post("/users", createUser);
+router.get("/:userId", getUser);
+router.get("/", getUsers);
+router.post("/", createUser);
 
 router.use("*", (req, res) => {
   res.status(404).send({ message: "Requested resource not found" });
