b79c39ac1af9a9e1c8dc1002ba20b0ce063ff6e0
Crii-CL <crisrecon14@gmail.com>
2023-05-17 16:42:44 -0400

    working on the user schema adding email and passwords

--- models/user.js
+++ models/user.js
@@ -18,6 +18,32 @@ const userSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+    minlength: 2,
+    maxlength: 30,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: "You must enter a valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 30,
+    validate: {
+      validator(v) {
+        return /(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z\d]/.test(v);
+      },
+      message: "You must enter a valid password",
+    },
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
