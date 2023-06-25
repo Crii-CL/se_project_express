0d955dbcc0a319b3186f3cf820de7ba39a241e83
Crii-CL <crisrecon14@gmail.com>
2023-06-25 02:35:54 -0400

    added the app.get required for the final project; must be removed after accepted

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,13 @@ const { validateUser } = require("../middlewares/validation");
 
 router.use("/items", clothingItems);
 router.use("/users", authorization, users);
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 router.post(
   "/signin",
   celebrate({
@@ -21,21 +28,7 @@ router.post(
   }),
   userLogin
 );
-router.post(
-  "/signup",
-  // celebrate({
-  //   body: Joi.object()
-  //     .keys({
-  //       name: Joi.string().required().min(2).max(30),
-  //       avatar: Joi.string().uri().required(),
-  //       email: Joi.string().required(),
-  //       password: Joi.string().required().min(5),
-  //     })
-  //     .unknown(true),
-  // }),
-  validateUser,
-  createUser
-);
+router.post("/signup", validateUser, createUser);
 
 router.use("*", (req, res) => {
   res.status(error.NOT_FOUND).send({ message: "Requested resource not found" });
