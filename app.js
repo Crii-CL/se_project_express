203a862e2abcb5bbe6c90ee52efe91f191dd26e4
Crii-CL <crisrecon14@gmail.com>
2023-10-02 19:14:44 -0400

    .

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
-const cors = require("cors");
 const { errors } = require("celebrate");
 const routes = require("./routes");
 const { handleErrorMiddleware } = require("./middlewares/errorHandler");
@@ -32,14 +31,13 @@ app.use(express.json());
 
 app.use(helmet());
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigins.join(", "));
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(requestLogger);
 
